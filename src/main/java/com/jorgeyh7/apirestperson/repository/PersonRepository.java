07d26a7ed892bd910d783f81c809424497ce9ded
jorge <yugito_is@hotmail.com>
2024-03-21 15:46:59 -0500

    fix anotation repository

--- src/main/java/com/jorgeyh7/apirestperson/repository/PersonRepository.java
+++ src/main/java/com/jorgeyh7/apirestperson/repository/PersonRepository.java
@@ -2,7 +2,9 @@ package com.jorgeyh7.apirestperson.repository;
 
 import com.jorgeyh7.apirestperson.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person,Long> {
 
     public Person findByUserAndPassword(String user, String password);
